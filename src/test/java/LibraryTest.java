e848da2e1e69c3d40e51754be7b8d388e5f7a844
WillMcBurney <pmcburn1@gmail.com>
2022-09-07 13:26:23 -0400

    added Equivalence test for checkout

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -34,4 +34,25 @@ public class LibraryTest {
         assertTrue(testBookCopies.containsKey(gardensOfTheMoon), "Test book no longer in Map");
         assertEquals(4, testBookCopies.get(gardensOfTheMoon), "Incorrect number of copies after add");
     }
+
+    @Test
+    public void checkOutEquivalenceTest() {
+        Map<Book, Integer> testBookCopies = new HashMap<>();
+        List<Patron> patronList = new ArrayList<>();
+        Book gardensOfTheMoon = new Book(1,
+                "Gardens Of The Moon: Book 1 of Malazan Book of the Fallen",
+                "Steven Erikson");
+        List<Book> patronCheckedOut = new ArrayList<>();
+        Patron testPatron = new Patron(12, "John", "Smith", patronCheckedOut);
+        Library testLibrary = new Library(testBookCopies, patronList);
+        testBookCopies.put(gardensOfTheMoon, 2);
+        patronList.add(testPatron);
+
+        testLibrary.checkOut(testPatron, gardensOfTheMoon);
+        assertEquals(1, testBookCopies.get(gardensOfTheMoon),
+                "Library has wrong number of copies of test book");
+        assertTrue(patronCheckedOut.contains(gardensOfTheMoon),
+                "Patron doesn't have test book in their checked out list");
+        assertEquals(1, patronCheckedOut.size(), "Patron doesn't have right number of books checked out");
+    }
 }
